c38a3ea4fff248c36df45a71ccdb69a6f2d261b0
ddkogit <git config --global user.email>
2024-03-03 12:03:20 +0545

    Accordion single selection completed

--- src/components/accordian/Accordion.jsx
+++ src/components/accordian/Accordion.jsx
@@ -5,34 +5,38 @@ import data from "./data";
 
 function Accordion() {
   const [selected, setSelected] = useState(null);
+  const [enableMultiSelection,setEnableMultiSelection] = useState(false);
 
+  console.log(enableMultiSelection);
   const handleSingleSelection = (id) => {
-    setSelected(id);
+    setSelected(id === selected ? null : id);
   };
+
+
   return (
     <div className="wrapper">
       <div className="accordion">
+
+        <button onClick={()=>setEnableMultiSelection(!enableMultiSelection)} className="multi-selection">
+          Enable Multi-Selection
+        </button>
         {data && data.length > 0 ? (
           data.map((item) => (
-            <div className="items" key={item.id}>
-              <div 
-              onClick={()=>handleSingleSelection(item.id)}
-              className="question">
-                <h3>
+            <div className="item" key={item.id}>
+              <div
+                className="title"
+                onClick={() => handleSingleSelection(item.id)}
+              >
+                <h3>{item.question}</h3>
 
-                {item.question}
-                </h3>
-                </div>
-              <span>+</span>
+                <span>+</span>
+              </div>
+
+              {selected === item.id ? <div>{item.answer}</div> : null}
             </div>
-
-           
           ))
-
-          
-
         ) : (
-          <div> No data found</div>
+          <div>Data not found</div>
         )}
       </div>
     </div>
